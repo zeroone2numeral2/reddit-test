9e86a73b0bfa71e44359fefaa5f61554eafe7ff1
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-03-12 16:06:22 +0100

    Added submission class to pseudocode

--- bot/jobs/post.py
+++ bot/jobs/post.py
@@ -5,7 +5,9 @@ from collections import namedtuple
 from pprint import pprint
 
 from utilities import u
-from database.models import
+from database.models import Channel
+from database.models import Subreddit
+from reddit import reddit
 from bot import Jobs
 from ..jobregistration import RUNNERS
 from config import config
@@ -22,26 +24,46 @@ def process_submissions(channel, subreddit, submissions):
             return submission
 
 
-@Jobs.add(RUNNERS.run_daily, time=datetime.time(hour=4, minute=0))
-# @Jobs.add(RUNNERS.run_repeating, interval=5000, first=0)
+# @Jobs.add(RUNNERS.run_daily, time=datetime.time(hour=4, minute=0))
+@Jobs.add(RUNNERS.run_repeating, interval=5000, first=0)
 def check_posts(bot, job):
     logger.info('job started at %s', u.now(string=True))
 
-    if now.hour > config.quite_hours.start and now.hour < config.quite_jours.end:
-        # don't post anything
+    now = u.now(string=False)
+
+    if config.quite_hours.enabled and (now.hour > config.quite_hours.start or now.hour < config.quite_jours.end):
+        logger.info('Quite hours (%d - %d): do not do anything (current hour: %d)',
+                    config.quite_hours.start, config.quite_hours.end, now.hour)
         return
 
     # PSEUDOCODE
+    channels = (
+        Channel.select()
+        .where(Channel.enabled)
+    )
     for channel in channels:
-        if not channel.enabled:
-            continue
+        logger.info('processing channel %d (%s)', channel.channel_id, channel.title)
 
-        for subreddit in channel.subreddits:
-            if time_difference(now(), sub.reddit.last_posted_submission_dt) < subreddit.max_frequency:
-                # do not post again if we posted less than the max frequency set
+        subreddits = (
+            Subreddit.select()
+            .where(Subreddit.channel == channel)
+        )
+
+        for subreddit in subreddits:
+            logger.info('channel %d: processing subreddit %s (r/%s)', channel.channel_id, subreddit.subreddit_id,
+                        subreddit.name)
+
+            if subreddit.last_posted_submission_dt and \
+                    ((u.now(string=False) - subreddit.last_posted_submission_dt).seconds / 60) < subreddit.max_frequency:
+                logger.info(
+                    'elapsed time (%s -- %s) is lower than max_frequency (%d minutes), continuing to next subreddit...',
+                    u.now(string=True),
+                    subreddit.last_posted_submission_dt.strftime('%d/%m/%Y %H:%M'),
+                    subreddit.max_frequency
+                )
                 continue
 
-            submissions = get_submissions(subreddit.name, limit=50)
+            submissions = reddit.subreddit(subreddit.name).hot(limit=config.praw.submissions_limit)
 
             submission = process_submissions(channel, subreddit, submissions)
             if not submission:
---
+++ reddit/__init__.py
@@ -0,0 +1,10 @@
+import praw
+
+from config import config
+
+
+reddit = praw.Reddit(
+    client_id=config.praw.client_id,
+    client_secret=config.praw.client_secret,
+    user_agent=config.praw.user_agent
+)
