fa0be6351527adfc0f6be94c666ef13f35a44b60
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-03-16 14:12:14 +0100

    New default template (closes #18)

--- const/post_template.py
+++ const/post_template.py
@@ -1,5 +1,2 @@
-DEFAULT_TEMPLATE = """\
-<b>{title_escaped}</b>
-
-<i>{sorting} • u/{author} • {elapsed_smart} ago • score: {score_dotted}</i>
-#{subreddit} • {thread_or_urls} ({num_comments_dotted})"""
+DEFAULT_TEMPLATE = """<b>{title_escaped}</b> #{subreddit}
+<i>{score_dotted} votes in {elapsed_smart}</i> • {thread_or_urls} ({num_comments})"""
