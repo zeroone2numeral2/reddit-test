e11cd6920494e0897e9ee0a115e51c8322d952ea
RememberTheAir <riccardomazzon96@gmail.com>
2019-03-19 11:59:50 +0000

    Db migrations improvements

--- database/migrations.py
+++ database/migrations.py
@@ -11,6 +11,12 @@ logging.basicConfig(format='[%(asctime)s][%(name)s] %(message)s', level=logging.
 logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser()
 
+MIGRATIONS = list(
+    (
+        '20191803pt1',
+    )
+)
+
 
 def main(db_filepath):
     db = peewee.SqliteDatabase(db_filepath, pragmas={'journal_mode': 'wal'})
@@ -21,19 +27,43 @@ def main(db_filepath):
     limit = peewee.IntegerField(null=True, default=25)
     ignore_if_newer_then = peewee.IntegerField(null=True)
 
+
+    migrations = {
+        '20190318pt1': [
+            migrator.add_column('subreddits', 'follow_quiet_hours', follow_quiet_hours),
+            migrator.add_column('subreddits', 'limit', limit),
+            migrator.add_column('subreddits', 'ignore_if_newer_then', ignore_if_newer_then)
+        ],
+        '20190318pt2': [
+            migrator.rename_column('subreddits', 'ignore_if_newer_then', 'ignore_if_newer_than')
+        ]
+    }
+
+    print('Available migrations:')
+    for migration_desc, _ in migrations.items():
+        print('- {}'.format(migration_desc))
+
+    selected_key = ''
+    while not migrations.get(selected_key, None):
+        selected_key = input('Select a migration: ')
+        if not migrations.get(selected_key, None):
+            print('Migration "{}" is not valid'.format(selected_key))
+
+    migrations_list = migrations[selected_key]
+
     logger.info('Starting migration....')
 
     try:
-        migrate(
-            # 18/03/2019 pt. 1
-            migrator.add_column('subreddits', 'follow_quiet_hours', follow_quiet_hours),
-            migrator.add_column('subreddits', 'limit', limit),
-            migrator.add_column('subreddits', 'ignore_if_newer_then', ignore_if_newer_then),
-            # 18/03/2019 pt. 2
-            migrator.rename_column('subreddits', 'ignore_if_newer_then', 'ignore_if_newer_than'),
-        )
-    except (peewee.DatabaseError, sqlite3.DatabaseError):
+        migrate(*migrations_list)
+    except sqlite3.DatabaseError as e:
         print('database file {} is encrypted or is not a database'.format(db_filepath))
+        print('sqlite3.DatabaseError:', str(e))
+        sys.exit(1)
+    except peewee.DatabaseError as e:
+        print('peewee.DatabaseError:', str(e))
+        sys.exit(1)
+    except ValueError as e:
+        print('ValueError:', str(e))
         sys.exit(1)
 
     logger.info('...migration completed')
