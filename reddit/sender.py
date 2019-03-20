8752563f3195d43dea78c5b1a3b057a12bcb370f
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-03-20 11:26:03 +0100

    Shortened variable name

--- reddit/sender.py
+++ reddit/sender.py
@@ -31,70 +31,70 @@ class Sender(dict):
     def __init__(self, bot, channel, subreddit, submission):
         super(Sender, self).__init__()
         self._bot = bot
-        self._submission = submission
+        self._s = submission
         self._channel = channel
         self._subreddit = subreddit
 
         self._sent_message = None
 
-        self._submission.is_image = False
-        self._submission.flair_with_space = ''
-        self._submission.nsfw = self._submission.over_18
-        self._submission.sorting = self._subreddit.sorting or 'hot'
-        self._submission.comments_url = 'https://www.reddit.com{}'.format(self._submission.permalink)
-        self._submission.score_dotted = u.dotted(self._submission.score or 0)
-        self._submission.num_comments_dotted = u.dotted(self._submission.num_comments or 0)
-        self._submission.text = None
-        self._submission.text_32 = None
-        self._submission.text_160 = None
-        self._submission.text_200 = None
-        self._submission.text_256 = None
+        self._s.is_image = False
+        self._s.flair_with_space = ''
+        self._s.nsfw = self._s.over_18
+        self._s.sorting = self._subreddit.sorting or 'hot'
+        self._s.comments_url = 'https://www.reddit.com{}'.format(self._s.permalink)
+        self._s.score_dotted = u.dotted(self._s.score or 0)
+        self._s.num_comments_dotted = u.dotted(self._s.num_comments or 0)
+        self._s.text = None
+        self._s.text_32 = None
+        self._s.text_160 = None
+        self._s.text_200 = None
+        self._s.text_256 = None
 
-        if self._submission.url.endswith(('.jpg', '.png')):
-            self._submission.is_image = True
-            self._submission.image_url = self._submission.url
-        elif re.search(r'.+imgur.com/\w+$', self._submission.url, re.I):
+        if self._s.url.endswith(('.jpg', '.png')):
+            self._s.is_image = True
+            self._s.image_url = self._s.url
+        elif re.search(r'.+imgur.com/\w+$', self._s.url, re.I):
             # check if the url is an url to an Imgur image even if it doesn't end with jpg/png
-            self._submission.is_image = True
-            self._submission.image_url = imgur.get_url(re.search(r'.+imgur.com/(\w+)$', self._submission.url, re.I).group(1))
+            self._s.is_image = True
+            self._s.image_url = imgur.get_url(re.search(r'.+imgur.com/(\w+)$', self._s.url, re.I).group(1))
 
-        if self._submission.link_flair_text is not None:
-            self._submission.flair_with_space = '[{}] '.format(self._submission.link_flair_text)
+        if self._s.link_flair_text is not None:
+            self._s.flair_with_space = '[{}] '.format(self._s.link_flair_text)
 
         # if the post is a textual post, it will contain a "thread" inline url. Otherwise it will contain the "url"
         # and "comments" inline urls
-        if self._submission.comments_url == self._submission.url:
-            self._submission.textual = True
-            self._submission.thread_or_urls = '<a href="{}">thread</a>'.format(self._submission.comments_url)
+        if self._s.comments_url == self._s.url:
+            self._s.textual = True
+            self._s.thread_or_urls = '<a href="{}">thread</a>'.format(self._s.comments_url)
         else:
-            self._submission.textual = False
-            self._submission.thread_or_urls = '<a href="{}">url</a> • <a href="{}">comments</a>'.format(
-                self._submission.url,
-                self._submission.comments_url
+            self._s.textual = False
+            self._s.thread_or_urls = '<a href="{}">url</a> • <a href="{}">comments</a>'.format(
+                self._s.url,
+                self._s.comments_url
             )
 
-        if self._submission.selftext:
-            self._submission.text = self._submission.selftext
-            self._submission.text_32 = self._submission.selftext[:32]
-            self._submission.text_160 = self._submission.selftext[:120]
-            self._submission.text_200 = self._submission.selftext[:200]
-            self._submission.text_256 = self._submission.selftext[:256]
+        if self._s.selftext:
+            self._s.text = self._s.selftext
+            self._s.text_32 = self._s.selftext[:32]
+            self._s.text_160 = self._s.selftext[:120]
+            self._s.text_200 = self._s.selftext[:200]
+            self._s.text_256 = self._s.selftext[:256]
 
-        created_utc_dt = datetime.datetime.utcfromtimestamp(self._submission.created_utc)
+        created_utc_dt = datetime.datetime.utcfromtimestamp(self._s.created_utc)
 
-        self._submission.title_escaped = u.escape(self._submission.title)
-        self._submission.created_utc_formatted = created_utc_dt.strftime('%d/%m/%Y, %H:%M')
+        self._s.title_escaped = u.escape(self._s.title)
+        self._s.created_utc_formatted = created_utc_dt.strftime('%d/%m/%Y, %H:%M')
 
-        self._submission.elapsed_seconds = (u.now() - created_utc_dt).seconds
-        self._submission.elapsed_minutes = self._submission.elapsed_seconds / 60
-        self._submission.elapsed_hours = self._submission.elapsed_minutes / 60
+        self._s.elapsed_seconds = (u.now() - created_utc_dt).seconds
+        self._s.elapsed_minutes = self._s.elapsed_seconds / 60
+        self._s.elapsed_hours = self._s.elapsed_minutes / 60
 
         # "n hours ago" if hours > 0, else "n minutes ago"
-        self._submission.elapsed_smart = u.elapsed_time_smart(self._submission.elapsed_seconds)
+        self._s.elapsed_smart = u.elapsed_time_smart(self._s.elapsed_seconds)
 
         self._submission_dict = dict()
 
-        for key in dir(self._submission):
+        for key in dir(self._s):
             val = getattr(submission, key)
             self._submission_dict[key] = val
             if KEY_MAPPER_DICT.get(key, None):
@@ -103,7 +103,7 @@ class Sender(dict):
 
     @property
     def submission(self):
-        return self._submission
+        return self._s
 
     @property
     def submission_dict(self):
@@ -115,7 +115,7 @@ class Sender(dict):
 
     @property
     def is_image(self):
-        return self._submission.is_image
+        return self._s.is_image
 
     def __getitem__(self, item):
         return self._submission_dict[item]
@@ -129,9 +129,9 @@ class Sender(dict):
         text = template.format(**self._submission_dict)
         # logger.info('post text: %s', text)
 
-        if self._submission.is_image and self._subreddit.send_images:
+        if self._s.is_image and self._subreddit.send_images:
             logger.info('post is an image: using send_photo()')
-            self._sent_message = self._send_image(self._submission.image_url, text, send_text_fallback=True)
+            self._sent_message = self._send_image(self._s.image_url, text, send_text_fallback=True)
         else:
             self._sent_message = self._send_text(text)
 
@@ -162,7 +162,7 @@ class Sender(dict):
     
     def register_post(self):
         Post.create(
-            submission_id=self._submission.id,
+            submission_id=self._s.id,
             subreddit=self._subreddit,
             channel=self._channel,
             message_id=self._sent_message.message_id if self._sent_message else None,
@@ -171,28 +171,28 @@ class Sender(dict):
     
     def register_ignored(self):
         Ignored.create(
-            submission_id=self._submission.id,
+            submission_id=self._s.id,
             subreddit=self._subreddit,
             ignored_at=u.now() if self._sent_message else None
         )
     
     def test_filters(self):
-        if self._subreddit.ignore_stickied and self._submission.stickied:
+        if self._subreddit.ignore_stickied and self._s.stickied:
             logger.info('tests failed: sticked submission')
             return False
         elif self._subreddit.images_only and not self.is_image:
             logger.info('tests failed: subreddit is not an image')
             return False
-        elif self._subreddit.min_score and isinstance(self._subreddit.min_score, int) and self._subreddit.min_score > self._submission.score:
-            logger.info('tests failed: not enough upvotes (%d/%d)', self._submission.score, self._subreddit.min_score)
+        elif self._subreddit.min_score and isinstance(self._subreddit.min_score, int) and self._subreddit.min_score > self._s.score:
+            logger.info('tests failed: not enough upvotes (%d/%d)', self._s.score, self._subreddit.min_score)
             return False
-        elif self._subreddit.allow_nsfw is not None and self._subreddit.allow_nsfw == False and self._submission.over_18:
+        elif self._subreddit.allow_nsfw is not None and self._subreddit.allow_nsfw == False and self._s.over_18:
             logger.info('tests failed: submission is NSFW')
             return False
         elif self._subreddit.ignore_if_newer_than \
                 and isinstance(self._subreddit.ignore_if_newer_than, datetime.datetime) \
-                and ((u.now() - self._submission.created_utc).seconds / 60) < self._subreddit.ignore_if_newer_than:
-            logger.info('tests failed: too new (submitted: %s)', self._submission.created_utc_formatted)
+                and ((u.now() - self._s.created_utc).seconds / 60) < self._subreddit.ignore_if_newer_than:
+            logger.info('tests failed: too new (submitted: %s)', self._s.created_utc_formatted)
             return False
         else:
             return True
