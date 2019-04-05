3c60d11f42265a2f28258bb0c85cf50a0b30a628
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-04-05 15:08:29 +0200

    Tests

--- tests/generic.py
+++ tests/generic.py
@@ -7,7 +7,7 @@ def dotted(number):
     return re.sub('(\d)(?=(\d{3})+(?!\d))', r'\1.', '{}'.format(number))
 
 
-print(dotted(1234546789))
+# print(dotted(1234546789))
 
 
 def string_to_minutes(string):
@@ -25,6 +25,27 @@ def string_to_minutes(string):
         return 'no match'
 
 
+def pretty_seconds(n_seconds):
+    if n_seconds < 60:
+        return '{}s'.format(n_seconds)
+
+    hours = int(n_seconds / 3600)
+    minutes = int(n_seconds / 60) % 60
+    seconds = n_seconds % 60
+
+    string = ''
+    if hours:
+        string += '{}h '.format(hours)
+
+    if minutes != 0 or (hours and seconds):
+        string += '{}m '.format(minutes)
+
+    if seconds or (minutes == 0 and hours == 0):
+        string += '{}s'.format(seconds)
+
+    return string
+
+
 strings = [
     '3h',
     '3 h 15 m',
@@ -36,7 +57,20 @@ strings = [
     'fcfg'
 ]
 
-for string in strings:
-    ret = string_to_minutes(string)
-    print(string, '--', ret)
 
+def main2():
+    for string in strings:
+        ret = string_to_minutes(string)
+        print(string, '--', ret)
+
+
+
+def main():
+    test = [0, 60, 3600, 62, 3672, 3602]
+    for t in test:
+        print(t, '-->', pretty_seconds(t))
+
+
+
+
+main()
