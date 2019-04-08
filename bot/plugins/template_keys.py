267214dc20185478f54014df17784953f1b797bd
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-04-08 14:35:48 +0200

    Refractored /ph command

--- bot/plugins/template_keys.py
+++ bot/plugins/template_keys.py
@@ -4,6 +4,10 @@ from telegram.ext import CommandHandler
 
 from bot import Plugins
 from utilities import d
+from utilities import u
+from database.models import Subreddit
+from reddit import Sender
+from reddit import reddit
 
 logger = logging.getLogger(__name__)
 
@@ -13,13 +17,22 @@ logger = logging.getLogger(__name__)
 @d.failwithmessage
 def subs_list(bot, update):
     logger.info('/placeholders command')
+
+    template_keys = list()
+    template_kv = dict()
+
+    sender = None
+    subreddit = Subreddit.select().get()
+    for submission in reddit.iter_submissions(subreddit.name, limit=1):
+        sender = Sender(bot, subreddit, submission)
+
+        template_keys = sender.template_keys
+        break
+
+    for key in template_keys:
+        if key not in ('STR_FIELD',):
+            val = getattr(sender.submission, key)
+            template_kv[key] = str(type(val))
     
-    try:
-        with open('template_keys.txt', 'r') as f:
-            lines = f.read().splitlines()
-    except FileNotFoundError:
-        update.message.reply_text('template_keys.txt not found')
-        return
-    
-    placeholders = ['<code>{' + line.strip() + '}</code>' for line in lines]
+    placeholders = ['<code>{}</code> {}'.format(key.strip(), u.html_escape(val)) for key, val in template_kv.items()]
     update.message.reply_html('\n'.join(placeholders))
