36bf1a3943a6f5fcf36e74acaf1ac7394263db94
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-05-08 15:40:13 +0200

    Imgur: check album size

--- reddit/downloaders/imgur.py
+++ reddit/downloaders/imgur.py
@@ -11,6 +11,10 @@ class InvalidAlbumUrl(Exception):
     pass
 
 
+class MediaTooMuch(Exception):
+    pass
+
+
 class Imgur(ImgurClient):
     def __init__(self, keys_from_config=False, *args, **kwargs):
         if not keys_from_config:
@@ -24,13 +28,21 @@ class Imgur(ImgurClient):
     def get_album_urls(self, album_id):
         return [image.link for image in self.get_album_images(album_id)]
 
-    def parse_album(self, album_url):
+    def parse_album(self, album_url, limit=0, raise_too_much=False):
         match = ALBUM_REGEX.search(album_url)
         if not match:
             raise InvalidAlbumUrl
 
         album_id = match.group(1)
-        return self.get_album_urls(album_id)
+        urls = self.get_album_urls(album_id)
+
+        if limit:
+            return urls[:limit]
+        else:
+            if len(urls) > 10 and raise_too_much:
+                raise MediaTooMuch
+            else:
+                return urls
 
     @staticmethod
     def is_album(url):
