5ded1ce49a968b00de03d761ae8f34eceb539659
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-05-14 11:55:18 +0200

    Added invite links to sender (closes #74)

--- reddit/sender.py
+++ reddit/sender.py
@@ -180,6 +180,10 @@ class Sender:
         # "n hours ago" if hours > 0, else "n minutes ago"
         self._s.elapsed_smart = u.elapsed_time_smart(self._s.elapsed_seconds)
 
+        self._s.index_channel_link = 'https://t.me/{}'.format(config.telegram.index) if config.telegram.get('index', None) else None
+        self._s.index_channel_username = '@{}'.format(config.telegram.index) if config.telegram.get('index', None) else None
+        self._s.channel_invite_link = self._subreddit.channel.invite_link or None
+
         self.gen_submission_dict()
 
     @property
