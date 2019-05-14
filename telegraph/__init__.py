319160f57254beed27a4919e0ab39d754542cb8d
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-05-14 17:07:50 +0200

    telegra.ph api

---
+++ telegraph/__init__.py
@@ -0,0 +1,1 @@
+from .telegraph import TelegraphAPI
---
+++ telegraph/telegraph.py
@@ -0,0 +1,43 @@
+from typing import List
+
+from telegraph import Telegraph
+
+
+class ListItem:
+    def __init__(self, created_on, subreddits, invite_link):
+        self.created_on = created_on
+        self.subreddits = subreddits
+        self.invite_link = invite_link
+
+
+class TelegraphAPI(Telegraph):
+    def __init__(self, *args, **kwargs):
+        Telegraph.__init__(self, *args, **kwargs)
+
+        self._list_items = list()
+
+    def update(self):
+        pass
+
+    def set(self, items: List[ListItem]) -> None:
+        for item in items:
+            if not isinstance(item, ListItem):
+                raise ValueError('All the list items must be of type ListItem')
+
+        self._list_items = items
+
+    def append(self, item: ListItem) -> None:
+        if not isinstance(item, ListItem):
+            raise ValueError('Item must be of type ListItem')
+
+        self._list_items.append(item)
+
+    def extend(self, items: List[ListItem]) -> None:
+        for item in items:
+            if not isinstance(item, ListItem):
+                raise ValueError('All the list items must be of type ListItem')
+
+        self._list_items.extend(items)
+
+    def reset(self):
+        self._list_items = list()
