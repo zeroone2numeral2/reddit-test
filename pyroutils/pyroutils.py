6e06f3168bcd47042f058e514d97917e6a845a8b
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-07-02 11:54:03 +0200

    Make custom pyrogram client return the sent message when uploading videos
    closes #82

--- pyroutils/pyroutils.py
+++ pyroutils/pyroutils.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class PyroClient(Client):
     def upload_video(self, chat_id, file_path, *args, **kwargs):
         try:
-            self.send_video(chat_id, file_path, *args, **kwargs)
+            return self.send_video(chat_id, file_path, *args, **kwargs)
         except ValueError:
             logger.warning('file size is too big for pyrogram')
             raise FileTooBig
