aaf8719142f6f5d53b8a1fccd11e313256442507
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-09-09 11:38:24 +0200

    /setchannel: work with multiple subreddits

--- bot/plugins/subreddit/subreddit_channel.py
+++ bot/plugins/subreddit/subreddit_channel.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from telegram.ext import ConversationHandler
 from telegram.ext import MessageHandler
@@ -14,35 +15,50 @@ from utilities import d
 
 logger = logging.getLogger(__name__)
 
-CHANNEL_SELECT = range(1)
+SUBREDDIT_SELECT, CHANNEL_SELECT = range(2)
 
 
 @d.restricted
 @d.failwithmessage
-def on_set_channel_command(_, update, args, user_data):
+def on_set_channel_command(_, update, args):
     logger.info('/subchannel command, args: %s', args)
-    
-    if not args:
-        update.message.reply_text('Usage: /subchannel [sub name]')
+
+    name_filter = args[0] if args else None
+
+    subreddits = Subreddit.get_list(name_filter=name_filter)
+    if not subreddits:
+        update.message.reply_text('Cannot find any subreddit (filter: {})'.format(name_filter))
         return ConversationHandler.END
-    
-    subreddit_name = u.normalize_sub_name(args[0])
-    
-    subreddit = Subreddit.fetch(subreddit_name)
-    if not subreddit:
-        logger.info('subreddit %s does not exist in the database', subreddit_name)
-        update.message.reply_text('Subreddit "r/{}" is not in the database, use /addsub to add one'.format(subreddit_name))
-        return ConversationHandler.END
+
+    reply_markup = Keyboard.from_list(['{}. /r/{} ({})'.format(s.id, s.name, s.channel.title) for s in subreddits])
+
+    update.message.reply_text('Select the subreddit (or /cancel):', reply_markup=reply_markup)
+
+    return SUBREDDIT_SELECT
+
+
+@d.restricted
+@d.failwithmessage
+def on_subreddit_selected(_, update, user_data):
+    logger.info('subreddit selected: %s', update.message.text)
+
+    subreddit_key = int(re.search(r'(\d+)\. .*', update.message.text, re.I).group(1))
+    logger.info('subreddit key: %d', subreddit_key)
+
+    subreddit = Subreddit.get(Subreddit.id == subreddit_key)
+
+    user_data['subreddit'] = subreddit
 
     channels_list = Channel.get_list()
     if not channels_list:
         update.message.reply_text('No saved channel. Use /addchannel to add a channel')
         return ConversationHandler.END
 
-    user_data['subreddit_peewee_obj'] = subreddit
-
     reply_markup = Keyboard.from_list(channels_list)
-    update.message.reply_text('Select the subreddit channel (or /cancel):', reply_markup=reply_markup)
+    update.message.reply_text(
+        'Selected subreddit: /r/{s.name} (channel: {s.channel.title}). Select the new channel, or /cancel:'.format(s=subreddit),
+        reply_markup=reply_markup
+    )
 
     return CHANNEL_SELECT
 
@@ -55,7 +71,7 @@ def on_channel_selected(_, update, user_data):
     channel_id = u.expand_channel_id(update.message.text)
     channel = Channel.get(Channel.channel_id == channel_id)
 
-    subreddit = user_data.pop('subreddit_peewee_obj')
+    subreddit = user_data.pop('subreddit')
     subreddit.channel = channel
     subreddit.save()
 
@@ -66,9 +82,11 @@ def on_channel_selected(_, update, user_data):
 
 @d.restricted
 @d.failwithmessage
-def on_cancel(_, update):
+def on_cancel(_, update, user_data):
     logger.info('conversation canceled with /cancel')
     update.message.reply_text('Operation aborted', reply_markup=Keyboard.REMOVE)
+
+    user_data.pop('subreddit', None)
 
     return ConversationHandler.END
 
@@ -88,10 +106,10 @@ def setchannel_conv_hanlder():
         entry_points=[CommandHandler(
             command=['subchannel', 'setchannel'],
             callback=on_set_channel_command,
-            pass_args=True,
-            pass_user_data=True
+            pass_args=True
         )],
         states={
+            SUBREDDIT_SELECT: [MessageHandler(Filters.text, callback=on_subreddit_selected, pass_user_data=True)],
             CHANNEL_SELECT: [
                 MessageHandler(Filters.text & Filters.regex(r'\d+\.\s.+'), callback=on_channel_selected,
                                pass_user_data=True),
@@ -99,7 +117,7 @@ def setchannel_conv_hanlder():
             ]
         },
         fallbacks=[
-            CommandHandler('cancel', on_cancel)
+            CommandHandler('cancel', on_cancel, pass_user_data=True)
         ]
     )
 
