1b956decf44d22661227a481dfb3c6e09a0084f5
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-09-20 11:46:15 +0200

    Fix

--- bot/plugins/channel/channel_list.py
+++ bot/plugins/channel/channel_list.py
@@ -32,7 +32,7 @@ def on_channels_list(bot, update):
     lines = list()
     non_public_channels = list()
     for channel in channels:
-        if not channel.get('public', None):
+        if channel.get('public', False):
             # do not post channels that are not public
             non_public_channels.append(channel['title'])
             continue
