42e5f19dbe557c147ccb3b5c1546d15c8a2a393d
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-10-08 11:33:08 +0200

    Load all the config keys in config.praw

--- reddit/__init__.py
+++ reddit/__init__.py
@@ -5,10 +5,4 @@ from .reddit import Reddit
 from config import config
 
 
-reddit = Reddit(
-    client_id=config.praw.client_id,
-    client_secret=config.praw.client_secret,
-    user_agent=config.praw.user_agent,
-    username=config.praw.username,
-    password=config.praw.password
-)
+reddit = Reddit(**config.praw)
