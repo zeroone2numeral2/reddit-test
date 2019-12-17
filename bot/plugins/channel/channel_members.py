81edd3da20111b176e28771ca3007ae653b322d7
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-12-17 11:50:01 +0100

    channel_memebrs.py: moved to ptb 12

--- bot/plugins/channel/channel_members.py
+++ bot/plugins/channel/channel_members.py
@@ -1,21 +1,19 @@
 import logging
 
-from telegram import Bot
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackContext
 from telegram.error import BadRequest
 from telegram.error import TelegramError
-from ptbplugins import Plugins
 
+from bot import mainbot
 from database.models import Channel
 from utilities import d
 
 logger = logging.getLogger(__name__)
 
 
-@Plugins.add(CommandHandler, command=['members'])
 @d.restricted
 @d.failwithmessage
-def on_top_members_command(bot: Bot, update):
+def on_top_members_command(update, context: CallbackContext):
     logger.info('/members command')
 
     channels = Channel.get_all()
@@ -28,7 +26,7 @@ def on_top_members_command(bot: Bot, update):
     channels_members = list()
     for channel in channels:
         try:
-            channel_count = bot.getChatMembersCount(channel.channel_id)
+            channel_count = context.bot.getChatMembersCount(channel.channel_id)
         except (TelegramError, BadRequest) as e:
             logger.warning('error while getting the chat: %s', e.message)
             errors.append((channel.title, e.message))
@@ -41,3 +39,6 @@ def on_top_members_command(bot: Bot, update):
     channels_string = '\n'.join(['{i}) {0}: {1}'.format(*c, i=i+1) for i, c in enumerate(channels_members)][:24])
     errors_string = '\n'.join(['{}: {}'.format(title, err) for title, err in errors])
     update.message.reply_text('{}\n\nErrors:\n{}'.format(channels_string, errors_string))
+
+
+mainbot.add_handler(CommandHandler('members', on_top_members_command))
