62218b76305301ec1f5dbf7e714973047fcc29a1
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-04-28 14:24:54 +0200

    Updated "subreddit_quarantine" to ptb 12

--- bot/plugins/subreddit/subreddit_quarantine.py
+++ bot/plugins/subreddit/subreddit_quarantine.py
@@ -1,6 +1,7 @@
 import logging
 
-from telegram.ext import CommandHandler
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
 from ptbplugins import Plugins
 
 from reddit import reddit
@@ -13,18 +14,18 @@ logger = logging.getLogger(__name__)
 @Plugins.add(CommandHandler, command=['optin', 'oi'], pass_args=True)
 @d.restricted
 @d.failwithmessage
-def optin_quarantined(_, update, args):
+def optin_quarantined(update: Update, context: CallbackContext):
     logger.info('/optin command')
 
     # Reference:
     # https://old.reddit.com/r/redditdev/comments/5vutzv/quaranopt_in_doesnt_seem_to_be_working/
     # https://old.reddit.com/r/redditdev/comments/asprwx/api_for_quarantined_subreddits/egw2n7l/
 
-    if len(args) < 1:
+    if len(context.args) < 1:
         update.message.reply_text('Pass the subreddit name')
         return
 
-    sub_name = args[0]
+    sub_name = context.args[0]
     try:
         subreddit = reddit.subreddit(sub_name)
         subreddit.quaran.opt_in()
