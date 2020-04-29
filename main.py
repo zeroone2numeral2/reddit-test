cee3a00b09499b2f4a7c67a7f005630785f8c449
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-04-29 17:23:59 +0200

    Run migrations when the script starts

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import logging
+
+import migrations
 import bot
+from config import config
+
+logging.getLogger('migrations').setLevel(logging.WARNING)
+
+migrations.main(config.sqlite.filename)
 
 bot.main()
