b30e7864af85cf37fbeb250bfa2851da49c00187
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-04-30 15:17:07 +0200

    /setdesc: filter by channel title (closes #108)

--- bot/plugins/channel/channel_desc.py
+++ bot/plugins/channel/channel_desc.py
@@ -1,7 +1,7 @@
 import logging
 
 # noinspection PyPackageRequirements
-from telegram import ParseMode
+from telegram import ParseMode, Update
 from telegram.ext import ConversationHandler, CallbackContext
 from telegram.ext import MessageHandler
 from telegram.ext import CommandHandler
@@ -255,13 +255,17 @@ def on_setdesc_channel_selected(update, context: CallbackContext):
 
 @d.restricted
 @d.failwithmessage
-def on_setdesc_command(update, _):
+def on_setdesc_command(update: Update, context: CallbackContext):
     logger.info('/setdesc command')
 
     channels_list = Channel.get_list()
     if not channels_list:
         update.message.reply_text('No saved channel. Use /addchannel to add a channel')
         return ConversationHandler.END
+
+    if len(context.args) > 0:
+        channel_title_filter = context.args[0].lower()
+        channels_list = [c for c in channels_list if channel_title_filter in c.lower()]
 
     reply_markup = Keyboard.from_list(channels_list)
     update.message.reply_text('Select the channel (or /cancel):', reply_markup=reply_markup)
