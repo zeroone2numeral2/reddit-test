6fe3dc03de394d72efba970172f90f0f3cbf22a4
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-04-30 15:36:33 +0200

    /remchannel: ability to filter list

--- bot/plugins/channel/channel_remove.py
+++ bot/plugins/channel/channel_remove.py
@@ -1,6 +1,7 @@
 import logging
 
-from telegram.ext import ConversationHandler
+from telegram import Update
+from telegram.ext import ConversationHandler, CallbackContext
 from telegram.ext import MessageHandler
 from telegram.ext import CommandHandler
 from telegram.ext import Filters
@@ -21,13 +22,17 @@ VALID_SUB_REGEX = r'(?:\/?r\/?)?([\w-]{3,22})'
 
 @d.restricted
 @d.failwithmessage
-def on_remchannel_command(update, _):
+def on_remchannel_command(update: Update, context: CallbackContext):
     logger.info('/remchannel command')
 
     channels_list = Channel.get_list()
     if not channels_list:
         update.message.reply_text('No saved channel. Use /addchannel to add a channel')
         return ConversationHandler.END
+
+    if len(context.args) > 0:
+        channel_title_filter = context.args[0].lower()
+        channels_list = [c for c in channels_list if channel_title_filter in c.lower()]
 
     reply_markup = Keyboard.from_list(channels_list)
     update.message.reply_text('Select the channel (or /cancel):', reply_markup=reply_markup)
