f53bf408b1f1ef537a2fec5f88ca24300f6e5948
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-05-19 15:00:40 +0200

    Removed unused handlers

--- bot/plugins/logs.py
+++ bot/plugins/logs.py
@@ -14,28 +14,6 @@ logger = logging.getLogger('handler')
 
 @d.restricted
 @d.failwithmessage
-def loglines_command(update, _):
-    logger.info('/loglines command')
-
-    dir_path = os.path.dirname(config.logging.filepath)
-
-    lines_list = list()
-    dir_files = [f for f in os.listdir(dir_path)][:50]
-    for file in dir_files:
-        file_path = os.path.join(dir_path, file)
-        if not re.search(r'.+\.log(?:\.\d+)?', file_path, re.I):
-            continue
-
-        with open(file_path) as f:
-            lines_list.append('{} - {}'.format(f.readline()[:25], str(file)))
-
-    text = '<code>{}</code>'.format('\n'.join(sorted(lines_list)))
-
-    update.message.reply_html(text)
-
-
-@d.restricted
-@d.failwithmessage
 def remffmpeglogs_command(update, _):
     logger.info('/remffmpeglogs command')
 
@@ -60,20 +38,6 @@ def remsubslogs_command(update, _):
         u.remove_file_safe(file_path)
 
     update.message.reply_text('Removed {} log files'.format(len(files)))
-
-
-@d.restricted
-@d.failwithmessage
-def getlog_command(update, context: CallbackContext):
-    logger.info('/getlog command')
-
-    file_path = config.logging.filepath
-    if context.args and re.search(r'^\d+$', context.args[0], re.I):
-        log_file_num = context.args[0]
-        file_path = file_path.replace('.log', '.log.{}'.format(log_file_num))
-
-    with open(os.path.normpath(file_path), 'rb') as f:
-        update.message.reply_document(f)
 
 
 mainbot.add_handler(CommandHandler(['remffmpeglogs'], remffmpeglogs_command))
