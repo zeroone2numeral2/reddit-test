27a7bf2a9b74424b2241f06eb41fcb9b5da9488c
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-06-18 16:07:38 +0200

    Updated default templates

--- const/post_template.py
+++ const/post_template.py
@@ -2,28 +2,28 @@ DEFAULT_TEMPLATE_2 = """<b>{title_escaped}</b>
 <i>{score_dotted} votes in {elapsed_smart}</i> • {thread_or_urls} ({num_comments})"""
 
 DEFAULT_TEMPLATE_1 = """<b>{title_escaped}</b>
-{score_dotted} votes in {elapsed_smart} • {num_comments} comments
+{score_dotted} votes ({upvote_perc}%) in {elapsed_smart} • {num_comments} comments
 
 {shortlink}"""
 
 DEFAULT_TEMPLATE_3 = """<b>{title_escaped}</b>
-<i>{score_dotted} votes in {elapsed_smart} • {num_comments} comments</i>
+<i>{score_dotted} votes ({upvote_perc}%) in {elapsed_smart} • {num_comments} comments</i>
 {url}
 
 {shortlink}"""
 
 DEFAULT_TEMPLATE_4 = """{hidden_url}<b>{title_escaped}</b>
-{score_dotted} votes in {elapsed_smart} • {num_comments} comments
+{score_dotted} votes ({upvote_perc}%) in {elapsed_smart} • {num_comments} comments
 
 {shortlink}"""
 
 DEFAULT_TEMPLATE_5 = """<b>{title_escaped}</b>
-{score_dotted} votes in {elapsed_smart} • {num_comments} comments
+{score_dotted} votes ({upvote_perc}%) in {elapsed_smart} • {num_comments} comments
 
 {shortlink} • #{subreddit}"""
 
 DEFAULT_TEMPLATE_6 = """{hidden_url}<b>{title_escaped}</b>
-{score_dotted} votes in {elapsed_smart} • {num_comments} comments
+{score_dotted} votes ({upvote_perc}%) in {elapsed_smart} • {num_comments} comments
 
 {shortlink} • #{subreddit}"""
 
