6711de89206210fde357bc071a59d8bc96ffd746
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-07-07 15:26:26 +0200

    Fixed issue with inline keyboards

--- bot/markups/inline_keyboard_markup.py
+++ bot/markups/inline_keyboard_markup.py
@@ -42,18 +42,23 @@ class InlineKeyboard:
     @staticmethod
     def post_buttons_with_labels(
             url_button_url=None,
-            url_button_label='url',
+            url_button_label=None,
             comments_button_url=None,
-            comments_button_label='comments'
+            comments_button_label=None
     ):
+        if url_button_label is None:
+            url_button_label = 'url'
+        if comments_button_label is None:
+            comments_button_label = 'comments'
+
         if url_button_url and comments_button_url and url_button_url.lower() == comments_button_url.lower():
             keyboard = [[InlineKeyboardButton(comments_button_label, url=comments_button_url)]]
             return InlineKeyboardMarkup(keyboard)
 
         keyboard = [[]]
-        if url_button_url:
+        if url_button_url is not None:
             keyboard[0].append(InlineKeyboardButton(url_button_label, url=url_button_url))
-        if comments_button_url:
+        if comments_button_url is not None:
             keyboard[0].append(InlineKeyboardButton(comments_button_label, url=comments_button_url))
 
         return InlineKeyboardMarkup(keyboard)
