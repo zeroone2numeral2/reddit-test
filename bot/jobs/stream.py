ae952fea88298be4159cc565f190d1fc6d9afdef
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-09-10 13:41:49 +0200

    Types

--- bot/jobs/stream.py
+++ bot/jobs/stream.py
@@ -212,7 +212,7 @@ def is_time_to_process(subreddit: Subreddit):
 @d.logerrors
 @d.log_start_end_dt
 # @db.atomic('EXCLUSIVE')  # http://docs.peewee-orm.com/en/latest/peewee/database.html#set-locking-mode-for-transaction
-def check_posts(context) -> JobResult:
+def check_posts(context: CallbackContext) -> JobResult:
     bot = context.bot
 
     with db.atomic():  # noqa
