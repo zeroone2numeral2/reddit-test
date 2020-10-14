99d6cee417eb95ce6242048c4fa37c936b26be04
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-10-14 14:49:53 +0200

    Fixed blocking issue with logging

--- reddit/sender.py
+++ reddit/sender.py
@@ -881,8 +881,8 @@ class Sender:
         elif self._subreddit.min_upvote_perc and self._s.upvote_perc < self._subreddit.min_upvote_perc:
             self.log.info(
                 'tests failed: submission\'s upvote ratio is not good enough (db: %d, submission: %d)',
-                self._subreddit.upvote_perc,
-                self._s.min_upvote_perc
+                self._subreddit.min_upvote_perc,
+                self._s.upvote_perc
             )
             return False
         elif self._subreddit.ignore_if_newer_than \
