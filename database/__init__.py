3f6b7641e5064ccfbffc08a94ce2937aa46bca10
zeroone2numeral2 <numeralzeroone@gmail.com>
2021-01-18 09:44:37 +0100

    Another comment about foreign keys (#176)

--- database/__init__.py
+++ database/__init__.py
@@ -5,6 +5,7 @@ from config import config
 # we disable foreign keys costraint for now, as the database FK costraint are kinda messed up, and to fix them
 # we have to create the database again (since FK costraint (ON DELETE and so on) are set during tables creation
 # and can't be updated afterwards
+# for reference: https://stackoverflow.com/q/1884818/13350541
 db = SqliteExtDatabase(config.sqlite.filename, pragmas={'journal_mode': 'wal', 'foreign_keys': 0})
 
 
