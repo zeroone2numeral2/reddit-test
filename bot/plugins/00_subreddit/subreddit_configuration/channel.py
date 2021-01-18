46322d69898cd9a024efb8ebeb4f2713b2f14a0b
zeroone2numeral2 <numeralzeroone@gmail.com>
2021-01-18 17:15:49 +0100

    Closes #178

--- bot/plugins/00_subreddit/subreddit_configuration/channel.py
+++ bot/plugins/00_subreddit/subreddit_configuration/channel.py
@@ -16,13 +16,17 @@ logger = logging.getLogger('handler')
 @d.restricted
 @d.failwithmessage
 @d.logconversation
-def subconfig_on_setchannel_command(update: Update, _):
+def subconfig_on_setchannel_command(update: Update, context: CallbackContext):
     logger.info('setchannel callback: %s', update.message.text)
 
     channels_list = Channel.get_list()
     if not channels_list:
         update.message.reply_text('No saved channel. Use /end and /addchannel to add a channel')
         return Status.WAITING_SUBREDDIT_CONFIG_ACTION
+
+    if len(context.args) > 0:
+        channel_title_filter = context.args[0].lower()
+        channels_list = [c for c in channels_list if channel_title_filter in c.lower()]
 
     reply_markup = Keyboard.from_list(channels_list)
     update.message.reply_text(
