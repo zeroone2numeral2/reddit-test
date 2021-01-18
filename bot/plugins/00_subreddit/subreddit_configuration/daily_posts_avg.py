1cf587c2846d8de95159ce88d836b68933296beb
zeroone2numeral2 <numeralzeroone@gmail.com>
2021-01-18 13:54:16 +0100

    Fixed /dailyavg conversation problem

--- bot/plugins/00_subreddit/subreddit_configuration/daily_posts_avg.py
+++ bot/plugins/00_subreddit/subreddit_configuration/daily_posts_avg.py
@@ -3,6 +3,7 @@ import logging
 from telegram import Update
 from telegram.ext import CallbackContext, ConversationHandler
 
+from bot.conversation import Status
 from database.models import Subreddit
 from database.queries import subreddit_job
 from utilities import d
@@ -19,3 +20,5 @@ def subconfig_on_avgdaily_command(update: Update, context: CallbackContext, subr
 
     daily_average = subreddit_job.average_daily_posts(subreddit)
     update.message.reply_text("Last 7 days' average daily posts: {}".format(daily_average))
+
+    return Status.WAITING_SUBREDDIT_CONFIG_ACTION
