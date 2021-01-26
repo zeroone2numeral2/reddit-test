ab878650922c22e61eaf62836559700485381d80
zeroone2numeral2 <numeralzeroone@gmail.com>
2021-01-26 11:49:11 +0100

    Fixed legend string

--- bot/plugins/03_channel_config/channel_configuration/update_pinned_message.py
+++ bot/plugins/03_channel_config/channel_configuration/update_pinned_message.py
@@ -40,7 +40,7 @@ FOOTER = """- number of daily posts: <b>~{}</b>
 ADDITIONAL_FOOTER_PRIVATE_CHANNEL = """- this channel's invite link <a href="{}">here</a>"""
 
 ADDITIONAL_FOOTER_SHORT_INFO_LEGEND = """<b>Legend</b>:
-<code>#</code>: current position on the sub's frontpage (<i>hot</i>)
+<code>#</code>: current position on the sub's frontpage (based on the sub sorting)
 <code>v</code>: current score (upvotes)
 <code>c</code>: number of comments
 <code>%</code>: upvotes ratio
